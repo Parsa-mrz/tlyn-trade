0bb05c4831f0af7697c11a6721769daf4a535744
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2025-05-12 20:47:49 +0330

    feat: :sparkles: Enhance OrderDTO documentation for clarity on parameters

--- backend/app/DTOs/OrderDTO.php
+++ backend/app/DTOs/OrderDTO.php
@@ -2,8 +2,19 @@
 
 namespace App\DTOs;
 
+/**
+ * Data Transfer Object for creating an Order.
+ */
 class OrderDTO
 {
+    /**
+     * Create a new OrderDTO instance.
+     *
+     * @param int $user_id The ID of the user placing the order.
+     * @param string $type The type of order ('buy' or 'sell').
+     * @param float $weight The weight of the gold in grams.
+     * @param float $price_per_gram The price per gram of gold in Rial.
+     */
     public function __construct(
         public readonly int $user_id,
         public readonly string $type,
