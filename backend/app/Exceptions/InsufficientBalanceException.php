659394e7d3dccbe1d58e08e5c2de617b1254c1ea
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2025-05-12 20:48:19 +0330

    feat: :sparkles: Improve documentation for InsufficientBalanceException class

--- backend/app/Exceptions/InsufficientBalanceException.php
+++ backend/app/Exceptions/InsufficientBalanceException.php
@@ -4,8 +4,18 @@ namespace App\Exceptions;
 
 use Exception;
 
+/**
+ * Exception thrown when a user tries to perform an action
+ * requiring more balance than is available in their wallet.
+ */
 class InsufficientBalanceException extends Exception
 {
+    /**
+     * Create a new InsufficientBalanceException instance.
+     *
+     * @param string $message The exception message (default: 'Insufficient balance').
+     * @param int $code The HTTP status code or custom error code (default: 422).
+     */
     public function __construct(string $message = 'Insufficient balance', int $code = 422)
     {
         parent::__construct($message, $code);
